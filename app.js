dc5d09c3f82732727bb614906a26f595f5e2df52
Gabriel <goliveira.damato@gmail.com>
2020-01-11 19:01:21 -0300

    Criando endpoints

--- app.js
+++ app.js
@@ -1,39 +1,75 @@
 const app = require("express")();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const uuidv1 = require("uuid/v1");
+
+const ignore = {_id: false, __v: false};
 
 app.use(bodyParser.json());
 
-mongoose.connect("mongodb+srv://gabrieldamato:senha@cluster0-3cyc4.mongodb.net/test?retryWrites=true&w=majority", {
+mongoose.connect("mongodb://localhost:27017/Produto", {
     useNewUrlParser: true, useUnifiedTopology: true
-}, () =>{
-    console.log("Banco de dados conectado");
+}, (erro) =>{
+    if (erro){
+        console.log("Ocorreu um erro" + erro);
+    }else{
+        console.log("Banco de dados conectado");
+    }
 })
 
 require("./models/Produto")
 const Produto = mongoose.model("Produto");
 
 app.post("/produto", (req,res) => {
-    var produto = new Produto({
-        nome: req.body.nome, fabricante: req.body.fabricante, preco:req.body.preco
+    let id = uuidv1();
+    let produto = new Produto({
+        id , nome: req.body.nome, fabricante: req.body.fabricante, preco:req.body.preco
     });
 
-    produto.save().then(() => {
-        console.log("Deu erro" + JSON.stringify(erro));
-        res.res.status(200).json(produto);
-        res.send();
+    produto.save().then((result) => {
+        console.log("Produto salvo com sucesso " + JSON.stringify(result));
+        res.status(200).json(produto);
     }).catch((erro) => {
         if(erro){
             console.log("Deu erro" + JSON.stringify(erro));
             throw erro;
         }
-        console.log("Deu erro" + JSON.stringify(erro));
+            console.log("Deu erro" + JSON.stringify(erro));
         res.statusCode = 417;
-        res.send();    
-    })
+    });
     console.log("Teste");
 });
 
+app.get("/produto", (req, res) => {
+    
+    Produto.find({}, ignore).then((result, erro) => {
+        if (erro) {
+            console.log("Deu erro" + JSON.stringify(erro));
+            res.status(500);
+        }else{
+            console.log("resultado " + JSON.stringify(result));
+            res.status(200).json(result);
+        }
+    });
+});
+
+app.delete("/produto/:id", (req, res) => {
+
+    let id = req.params.id;
+
+    Produto.deleteOne({id}).then((result, erro) => {
+        if (erro) {
+            console.log("Deu erro" + erro);
+            res.status(500);
+        }else{
+            if (result.deletedCount > 0){
+                console.log("Resultado " + JSON.stringify(result));
+                res.status(200).json({mensagem: "Produto de ID = " + id + "removido com sucesso"});
+            }
+        }
+    })
+})
+
 app.listen(8080,() => {
     console.log("API rodando!");
-})
+});
--- models/Produto.js
+++ models/Produto.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.model("Produto", {
+    id:{type: String},
     nome:{type: String},
     fabricante:{type: String},
     preco:{type: Number}
